952c4309930c6be30ca5f07d6a9c4678b4e11db3
on1ine <on1ine@ya.ru>
2019-11-03 11:05:56 +0700

    variables

---
+++ variables/main.js
@@ -0,0 +1,37 @@
+// Задача №1
+console.log('Задача №1\n');
+
+let algebra = 5;
+let geography = 4;
+let physics = 4;
+
+let averageRating = (algebra + geography + physics) / 3;
+
+console.log(averageRating);
+
+console.log('\n---------------------------------------------\n');
+
+
+// Задача №2
+console.log('Задача №2\n');
+
+let myName = 'Антон';
+let message = `Привет, мир! Меня зовут ${myName}`;
+
+console.log(message);
+
+console.log('\n---------------------------------------------\n');
+
+
+// Задача №3
+console.log('Задача №3\n');
+
+let x = 2;
+let y = 22;
+let z = 0;
+
+let result = x * y + 5 * z + x - 1;
+
+console.log(result);
+
+console.log('\n---------------------------------------------\n');
