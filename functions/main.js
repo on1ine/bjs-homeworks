c4276340cc40d5698a2bab06d29c3de50f5d5713
wan <on1ine@ya.ru>
2019-11-17 18:57:39 +0700

    wrappers-for-primitives

---
+++ functions/main.js
@@ -0,0 +1,133 @@
+"use strict"
+
+// Задача №1
+console.log('Задача №1\n');
+
+function getSolutions( a, b, c ) {
+	let d = b ** 2 - 4 * a * c;
+	let result = {
+		d
+	} 
+		if (d  === 0) {
+			let x1 = -b / (2 * a);
+			result.roots = [x1];
+		}
+		if (d > 0) {
+			let x1 = (-b + Math.sqrt(d)) / (2 * a), x2 = (-b - Math.sqrt(d)) / (2 * a);
+			result.roots = [x1, x2];
+		}
+		return result;		
+}
+
+// console.log(getSolutions(2, 4, -3));
+
+function showSolutionsMessage(a, b, c) {
+	let result = getSolutions(a, b, c);
+	console.log(`Вычисляем корни квадратного уравнения ${a}x² + ${b}x + ${c}`);
+	console.log(`Значение дискриминанта: ${result.d}`);
+
+	if (result.d === 0) {
+		console.log(`Уравнение имеет один корень X₁ = ${result.roots[0]}`);
+	} else if (result.d > 0) {
+		console.log(`Уравнение имеет два корня. X₁ = ${result.roots[0]}, X₂ = ${result.roots[1]}`);
+	}	else {
+		console.log('Уравнение не имеет вещественных корней');
+	}
+}
+
+showSolutionsMessage(1, 2, 3);
+showSolutionsMessage(7, 20, -3);
+showSolutionsMessage(2, 4, 2);
+
+console.log('\n---------------------------------------------\n');
+
+
+// Задача №2
+console.log('Задача №2\n');
+
+function getAverageScore(data) {
+  const result = {};
+  let totalMark = 0;
+
+  for (const subject in data) {
+    const averageMark = getAverageArray(data[subject]);
+    result[subject] = averageMark;
+    totalMark += averageMark;
+  }
+
+  result.average = totalMark / Object.keys(data).length;
+  return result;
+}
+
+function getAverageArray(arr) {
+  let totalScore = 0;
+
+  for (let el of arr) {
+    totalScore += el;
+  }
+
+  return totalScore / arr.length;
+}
+
+console.log(getAverageScore({
+  algebra: [2, 4, 5, 2, 3, 4],
+  geometry: [2, 4, 5],
+  russian: [3, 3, 4, 5],
+  physics: [5, 5],
+  music: [2, 2, 6],
+  english: [4, 4, 3],
+  poetry: [5, 3, 4],
+  chemistry: [2],
+  french: [4, 4]
+}));
+
+
+console.log('\n---------------------------------------------\n');
+
+
+// Задача №3
+console.log('Задача №3\n');
+
+function getPersonData(secretData) {
+  const result = {};
+  
+  for (let key in secretData) {
+    let newKey;
+
+    if (key === 'aaa') {
+      newKey = 'firstName';
+    } else {
+      newKey = 'lastName';
+    }
+
+    result[newKey] = decoder(secretData[key]);
+  }
+
+  return result;
+}
+
+  function decoder(code) {
+    return code ? 'Эмильо' : 'Родриго';
+  }
+
+console.log(getPersonData({
+  aaa: 0,
+  bbb: 0
+}));
+
+console.log(getPersonData({
+  aaa: 1,
+  bbb: 0
+}));
+
+console.log(getPersonData({
+  aaa: 0,
+  bbb: 1
+}));
+
+console.log(getPersonData({
+  aaa: 1,
+  bbb: 1
+}));
+
+console.log('\n---------------------------------------------\n');
--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,28 +1,53 @@
-function calculateMortgage() {
-    let percent = window.percent.value;
-    let contribution = window.contribution.value;
-    let amount = window.amount.value;
-    let date = window.date.value;
+'use strict';
 
-    let result = calculateTotalMortgage(percent, contribution, amount, date);
-    let span = window.mortageResult;
-    span.textContent = result;
+function calculateMortgage() {
+	let percent = window.percent.value;
+	let contribution = window.contribution.value;
+	let amount = window.amount.value;
+	let date = window.date.value;
+
+	let result = calculateTotalMortgage(percent, contribution, amount, date);
+	let span = window.mortageResult;
+	span.textContent = result;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
+	let arg = {
+		percent,
+		contribution,
+		amount
+	};
+	for (let key in arg) {
+		if (isNaN(Number(arg[key]))) {
+			let result = `Параметр ${key} содержит неправильное значение ${arg[key]}`;
+			console.log(result);
+			return result;
+		}
+	}
 
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+	percent = Number(percent);
+	contribution = Number(contribution);
+	amount = Number(amount);
+	date = new Date(date);
+
+	let currentDate = new Date();
+	let months = date.getMonth() - currentDate.getMonth() + (12 * (date.getFullYear() - currentDate.getFullYear()));
+
+	let percentRate = (percent / 100) / 12;
+	let monthlyPayment = (amount - contribution) * (percentRate + percentRate / (((1 + percentRate) ** months) - 1));
+	return (monthlyPayment * months).toFixed(2);
 }
 
 function sayHello() {
-    let name = window.personName.value;
-    let greeting = getGreeting(name);
-    let span = window.helloResult;
-    span.textContent = greeting;
+	let name = window.personName.value;
+	let greeting = getGreeting(name);
+	let span = window.helloResult;
+	span.textContent = greeting;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
+	name = name ? name : 'Аноним';
+
+	let greeting = `Привет, мир! Меню зовут ${name}.`;
+	return greeting;
 }
