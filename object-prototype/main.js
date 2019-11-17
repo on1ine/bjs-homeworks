1ee33463e03ec1102cbe4ecb105959d6488a5411
wan <on1ine@ya.ru>
2019-11-17 21:00:48 +0700

    object-prototype

--- object-prototype/main.js
+++ object-prototype/main.js
@@ -1,39 +1,57 @@
+'use strict';
+
 function initCheckBirthday() {
-    const birthday = document.getElementById('birthday').value;
+	const birthday = document.getElementById('birthday').value;
 
-    const result = checkBirthday(birthday) ? "Да" : "Нет";
+	const result = checkBirthday(birthday) ? "Да" : "Нет";
 
-    document.getElementById('disclaimer').innerHTML = result;   
+	document.getElementById('disclaimer').innerHTML = result;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №1 писать здесь
+	let now = new Date().getTime();
+	birthday = new Date(birthday).getTime();
+	let diff = (now - birthday);
+	let age = diff / 31557600000;
+	return age > 18;
 }
 
+
 function initPrintAnimalSound() {
-    const animal = {
-        sound: 'grrrr',
-    };
+	const animal = {
+		sound: 'grrrr',
+	};
 
-    const result = getAnimalSound(animal);
+	const result = getAnimalSound(animal);
 
-    document.getElementById('sound').innerHTML = result;   
+	document.getElementById('sound').innerHTML = result;
 }
 
 function getAnimalSound(animal) {
-    // код для задачи №2 писать здесь
+	let sound = animal.sound;
+	if (typeof animal === 'undefined') {
+		return null;
+	} else
+		return sound;
 }
 
 function initCalculateStatement() {
-    for (let idx = 0; idx < 3; idx++) {
-        const marks = document.getElementById('learner-' + idx).value.split(',');
+	for (let idx = 0; idx < 3; idx++) {
+		const marks = document.getElementById('learner-' + idx).value.split(',');
 
-        const average = getAverageMark(marks);
+		const average = getAverageMark(marks);
 
-        document.getElementById('learner-' + idx + '-average').innerHTML = average;
-    }
+		document.getElementById('learner-' + idx + '-average').innerHTML = average;
+	}
 }
 
 function getAverageMark(marks) {
-    // код для задачи №3 писать здесь
+	let average = 0;
+
+	for (let mark of marks) {
+		average += parseFloat(mark);
+	}
+
+	let roundedAverage = Math.round(average / marks.length);
+	return roundedAverage;
 }
